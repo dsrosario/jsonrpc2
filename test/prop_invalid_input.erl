a96f72dadeb99c99aab61a6bd023466e8a37dc43
Dinis Rosario <dinis.rosario@gmail.com>
2019-02-25 19:54:03 +0000

    Test invalid input binary

---
+++ test/prop_invalid_input.erl
@@ -0,0 +1,23 @@
+-module(prop_invalid_input).
+-include_lib("proper/include/proper.hrl").
+
+%%%%%%%%%%%%%%%%%%
+%%% Properties %%%
+%%%%%%%%%%%%%%%%%%
+prop_test() ->
+    ?FORALL(InvalidBinary, utf8(),
+        begin
+            {reply, Reply} = jsonrpc2:handle(InvalidBinary, fun rpc_handler/2),
+            Json = jsx:decode(Reply, [return_maps]),
+            is_expected_reply(Json)
+        end).
+
+%%%%%%%%%%%%%%%
+%%% Helpers %%%
+%%%%%%%%%%%%%%%
+rpc_handler(_, _) ->
+    throw(unexpected_function_call).
+
+is_expected_reply(#{<<"jsonrpc">> := <<"2.0">>,
+                    <<"error">> := #{<<"code">> := -32700}}) -> true;
+is_expected_reply(_) -> false.
